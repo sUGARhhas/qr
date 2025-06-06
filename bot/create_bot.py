e3f6f515d3935418437744aa39f41280a17a6973
sUGARhhas <122264822+sUGARhhas@users.noreply.github.com>
2025-06-06 15:22:08 +0900

    Add files via upload

--- bot/create_bot.py
+++ bot/create_bot.py
@@ -4,5 +4,5 @@ from aiogram.fsm.storage.memory import MemoryStorage
 
 storage = MemoryStorage()
 
-bot = Bot(token="7873300445:AAGigBhxKPetZp8tCpqgOHQx2ndv2gWQSxc")
+bot = Bot(token="5917276984:AAFc697QqPrJlH_cM_Owulha33tIuO4Vrhw")
 dp = Dispatcher(storage=storage)
